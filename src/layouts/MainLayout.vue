8c925383223a5da27009deeaed904077a857f6ac
DTK <cristhiandanielcoaquira@gmail.com>
2023-06-27 14:28:22 -0400

    Implement MainLayout and install required libraries

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="container">
+    <HeaderBar></HeaderBar>
     <router-view />
+    <FooterBar></FooterBar>
   </div>
 </template>
 <script>
+import HeaderBar from "@/components/Header/HeaderBar.vue";
+import FooterBar from "@/components/Footer/FooterBar.vue";
 export default {
   name: "MainLayout",
+  components: { HeaderBar, FooterBar },
 };
 </script>
