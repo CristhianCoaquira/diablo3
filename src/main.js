24dfde7d924650faeb401eb96ab6fa4386ce18d9
DTK <cristhiandanielcoaquira@gmail.com>
2023-07-23 13:13:51 -0400

    separate plugins logic

--- src/main.js
+++ src/main.js
@@ -1,43 +1,12 @@
 import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
-import "./plugins/bootstrap-vue";
 import "./assets/css/main.scss";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-} from "@fortawesome/free-solid-svg-icons";
-import {
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-);
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+import "./plugins/index";
 
 Vue.config.productionTip = false;
 
