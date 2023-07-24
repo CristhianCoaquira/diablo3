dcf0935e43afb1a70a803ba84a4173f387b4d93f
DTK <cristhiandanielcoaquira@gmail.com>
2023-07-24 12:59:48 -0400

    Implement routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,44 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home/IndexView.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
+const routerOptions = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+  },
+  {
+    path: "/region/:region/profile/:battleTag",
+    name: "Profile",
+  },
+  {
+    path: "/region/:region/profile/:battleTag/hero/:heroId",
+    name: "Hero",
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About/IndexView.vue"),
+  },
+  {
+    path: "/error",
+    name: "Error",
+  },
+  {
+    path: "*",
+    redirect: { name: "Home" },
   },
 ];
+
+const routes = routerOptions.map((r) => {
+  return {
+    ...r,
+    component: () =>
+      import(
+        /* webpackChunkName: "[request]" */ `@/views/${r.name}/IndexView.vue`
+      ),
+  };
+});
 
 const router = new VueRouter({
   mode: "history",
