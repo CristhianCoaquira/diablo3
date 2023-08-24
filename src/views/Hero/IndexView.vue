70bfe5abc94723f11f0effe13a77168dc5704447
DTK <cristhiandanielcoaquira@gmail.com>
2023-08-24 17:06:13 -0400

    Implement hero detail module

--- src/views/Hero/IndexView.vue
+++ src/views/Hero/IndexView.vue
@@ -1,9 +1,116 @@
 <template>
-  <div>
-    <h1>Hero</h1>
+  <div class="hero-view">
+    <BaseLoading v-if="isLoadingHero" />
+    <HeroDetailHeader v-if="hero" :detail="detailHeader" />
+    <b-row>
+      <b-col md="12" lg="8" order-lg="2">
+        <BaseLoading v-if="isLoadingItems" />
+      </b-col>
+      <b-col md="12" lg="4" order-lg="1">
+        <template v-if="hero">
+          <HeroAttributes :attributes="detailStats" />
+          <HeroSkills :skills="hero.skills" />
+        </template>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-export default {};
+import setError from "@/mixins/setError";
+import BaseLoading from "@/components/BaseLoading.vue";
+import HeroDetailHeader from "@/views/Hero/HeroDetailHeader.vue";
+import HeroAttributes from "./HeroAttributes/IndexView.vue";
+import HeroSkills from "./HeroSkills/IndexView.vue";
+// import HeroItems from "./HeroItems/IndexView.vue";
+import { getApiDetailedHeroItems, getApiHero } from "@/api/search";
+
+export default {
+  name: "HeroView",
+  mixins: [setError],
+  components: {
+    BaseLoading,
+    HeroDetailHeader,
+    HeroAttributes,
+    HeroSkills,
+    // HeroItems,
+  },
+  data() {
+    return {
+      isLoadingHero: false,
+      isLoadingItems: false,
+      hero: null,
+      items: null,
+    };
+  },
+  computed: {
+    detailHeader() {
+      const {
+        name,
+        class: classSlug,
+        gender,
+        level,
+        hardcore,
+        seasonal,
+        paragonLevel,
+        alive,
+        seasonCreated,
+      } = this.hero;
+
+      return {
+        name,
+        classSlug,
+        gender,
+        level,
+        hardcore,
+        seasonal,
+        paragonLevel,
+        alive,
+        seasonCreated,
+      };
+    },
+    detailStats() {
+      // Devuelve el contenido de stats y agrega classSlug
+      return { ...this.hero.stats, classSlug: this.hero.class };
+    },
+  },
+  created() {
+    this.isLoadingHero = true;
+    this.isLoadingItems = true;
+    const { region, battleTag: account, heroId } = this.$route.params;
+
+    getApiHero({ region, account, heroId })
+      .then(({ data }) => {
+        this.hero = data;
+      })
+      .catch((err) => {
+        this.hero = null;
+        const errObj = {
+          routeParams: this.$route.params,
+          message: err.message,
+        };
+        if (err.response) {
+          errObj.data = err.response.data;
+          errObj.status = err.response.status;
+        }
+        this.setApiErr(errObj);
+        this.$router.push({ name: "Error" });
+      })
+      .finally(() => {
+        this.isLoadingHero = false;
+      });
+
+    getApiDetailedHeroItems({ region, account, heroId })
+      .then(({ data }) => {
+        this.items = data;
+      })
+      .catch((err) => {
+        this.items = null;
+        console.log(err);
+      })
+      .finally(() => {
+        this.isLoadingItems = false;
+      });
+  },
+};
 </script>
