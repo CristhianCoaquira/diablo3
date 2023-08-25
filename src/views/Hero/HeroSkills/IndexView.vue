11867188a4ac10f8f4b2ee144a11b6f5be87afcd
DTK <cristhiandanielcoaquira@gmail.com>
2023-08-25 15:09:30 -0400

    Implement async components and keep alive

--- src/views/Hero/HeroSkills/IndexView.vue
+++ src/views/Hero/HeroSkills/IndexView.vue
@@ -3,26 +3,57 @@
     <h2 class="font-diablo">Skills</h2>
     <hr class="bg-white" />
 
-    <ActiveSkills :skills="skills.active" />
-    <hr />
-    <PassiveSkills :skills="skills.passive" />
+    <b-nav pills small>
+      <b-nav-item
+        :active="!isPassiveSkillsActive"
+        @click="changeComponent('ActiveSkills')"
+        >Active</b-nav-item
+      >
+      <b-nav-item
+        :active="isPassiveSkillsActive"
+        @click="changeComponent('PassiveSkills')"
+        >Passive</b-nav-item
+      >
+    </b-nav>
+    <keep-alive>
+      <component :is="activeComponent" :skills="componentProps" />
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import ActiveSkills from "./ActiveSkills.vue";
-import PassiveSkills from "./PassiveSkills.vue";
-
 export default {
   name: "HeroSkills",
   components: {
-    ActiveSkills,
-    PassiveSkills,
+    ActiveSkills: () =>
+      import(/* webpackChunkName: "ActiveSkills" */ "./ActiveSkills.vue"),
+    PassiveSkills: () =>
+      import(/* webpackChunkName: "PassiveSkills" */ "./PassiveSkills.vue"),
   },
   props: {
     skills: {
       required: true,
       type: Object,
+    },
+  },
+  computed: {
+    componentProps() {
+      return this.activeComponent === "ActiveSkills"
+        ? this.skills.active
+        : this.skills.passive;
+    },
+    isPassiveSkillsActive() {
+      return this.activeComponent === "PassiveSkills";
+    },
+  },
+  data() {
+    return {
+      activeComponent: "ActiveSkills",
+    };
+  },
+  methods: {
+    changeComponent(component) {
+      this.activeComponent = component;
     },
   },
 };
