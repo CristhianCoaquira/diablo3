ee9995152aec42d171fa801cf6c843aae30e3e70
DTK <cristhiandanielcoaquira@gmail.com>
2023-08-29 11:42:06 -0400

    install and configure jest for test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,24 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mount, createLocalVue } from "@vue/test-utils";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+import HeaderBar from "@/components/Header/HeaderBar.vue";
+import Vuex from "vuex";
+import router from "@/router/index";
+import store from "@/store/index";
+
+import "@/plugins/index";
+import "@/directives/index";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const wrapper = mount(HeaderBar, {
+  store,
+  router,
+  localVue,
+});
+const vm = wrapper.vm;
+describe("Check view is loaded correctly", () => {
+  test("Check header title is equal to enviorement variable", () => {
+    expect(vm.title).toBe(process.env.VUE_APP_TITLE);
   });
 });
